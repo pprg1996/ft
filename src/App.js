71bf8508ccc5bccc2a3b44af61e951e936658012
pprg1996 <pprg1996@gmail.com>
2018-05-15 19:07:00 -0400

    m

--- src/App.js
+++ src/App.js
@@ -1,24 +1,82 @@
 import React, { Component } from "react";
-import { Link, Route } from "react-router-dom";
-import "./App.css";
-import Maxes from "./screens/maxes";
+import { Typography, Paper, TextField, Button, List, IconButton } from "material-ui";
+import { ListItem, ListItemText, ListItemSecondaryAction } from "material-ui/List"
+import { Delete } from "@material-ui/icons";
+import { withStyles } from "material-ui/styles";
 
-class App extends Component {
+const styles = theme =>
+    console.log(theme) || {
+        root: {
+            margin: "20px auto",
+            padding: 20,
+            maxWidth: 400
+        },
+        form: {
+            display: "flex",
+            alignItems: "baseline",
+            justifyContent: "space-evenly"
+        }
+    }
+
+export default withStyles(styles)(class App extends Component {
+
+    state = {
+        exercises: [{ id: 1, title: 'Bench Press' },
+        { id: 2, title: 'Deadlift' },
+        { id: 3, title: 'Squats' }],
+        title: ""
+    }
+
+    handleChange = ({ target: { name, value } }) => {
+        this.setState({ [name]: value });
+    }
+
+    handleCreate = e => {
+        e.preventDefault();
+        if (this.state.title) {
+            this.setState(({ exercises, title }) => (
+                {
+                    exercises: [
+                        ...exercises, {
+                            title, id: Date.now()
+                        }
+                    ],
+                    title: ""
+                }
+            ))
+        }
+    }
+
+    handleDelete = (id) => {
+        this.setState(({ exercises }) => ({ exercises: exercises.filter(ex => (ex.id != id)) }))
+    }
+
     render() {
-        return (
-            <div id="appDiv">
-                <div id="sideBar">
-                    <ul id="sideBarList">
-                        <li><Link to="/maxes">Maxes</Link></li>
-                    </ul>
-                </div>
+        const { title } = this.state;
+        const { classes } = this.props;
 
-                <div id="content">
-                    <Route path="/maxes" component={Maxes} />
-                </div>
-            </div>
+        return (
+            <Paper className={classes.root}>
+                <Typography variant="display1" align="center" gutterBottom >
+                    Exercises
+            </Typography>
+
+                <form className={classes.form} onSubmit={this.handleCreate}>
+                    <TextField name="title" label="Exercise" value={title} margin="normal" onChange={this.handleChange} />
+                    <Button type="submit" color="primary" variant="raised" >Create</Button>
+                </form>
+
+                <List>
+                    {this.state.exercises.map((exercise) => (
+                        <ListItem key={exercise.id}>
+                            <ListItemText primary={exercise.title} />
+                            <ListItemSecondaryAction>
+                                <IconButton color="primary" onClick={() => { this.handleDelete(exercise.id) }}><Delete /></IconButton>
+                            </ListItemSecondaryAction>
+                        </ListItem>
+                    ))}
+                </List>
+            </Paper>
         );
     }
-}
-
-export default App;
+})
--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App"
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import "./index.css";
+import App from "./App";
 
-ReactDOM.render(<BrowserRouter>< App /></BrowserRouter>, document.querySelector("#root"));
+ReactDOM.render(< App />, document.querySelector("#root"));
